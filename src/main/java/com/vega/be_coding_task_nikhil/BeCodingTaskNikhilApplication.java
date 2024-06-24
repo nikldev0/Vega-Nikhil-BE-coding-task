46ac973e508fedd59264c7481276579053622354
nikldev0 <nikhil.kanukuntla@protonmail.com>
2024-06-24 19:16:48 +0100

    set up application instance and properties

---
+++ src/main/java/com/vega/be_coding_task_nikhil/BeCodingTaskNikhilApplication.java
@@ -0,0 +1,13 @@
+package com.vega.be_coding_task_nikhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCodingTaskNikhilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCodingTaskNikhilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vega/be_coding_task_nikhil/BeCodingTaskNikhilApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vega.be_coding_task_nikhil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class BeCodingTaskNikhilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
