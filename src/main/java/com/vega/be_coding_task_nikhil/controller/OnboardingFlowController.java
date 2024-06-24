ce2c313256d32c2fdd1da730d5a98db418303784
nikldev0 <nikhil.kanukuntla@protonmail.com>
2024-06-24 23:40:16 +0100

    add @Validated annotation to controller

--- src/main/java/com/vega/be_coding_task_nikhil/controller/OnboardingFlowController.java
+++ src/main/java/com/vega/be_coding_task_nikhil/controller/OnboardingFlowController.java
@@ -18,6 +18,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/onboarding-flows")
+@Validated
 @Tag(name = "OnboardingFlowController", description = "The Onboarding Flow API")
 public class OnboardingFlowController {
 
